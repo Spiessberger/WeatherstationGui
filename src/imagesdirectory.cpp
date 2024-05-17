f2487980667d6017798c5a509dbfab90a970dd5e
Daniel Spießberger <daniel.spiessberger@sequality.at>
2024-05-17 13:04:39 +0200

    add class that loads images from a directory

---
+++ src/imagesdirectory.cpp
@@ -0,0 +1,60 @@
+#include "imagesdirectory.h"
+
+#include <QDir>
+#include <QFile>
+
+namespace wsgui
+{
+
+ImagesDirectory::ImagesDirectory(QObject* parent)
+    : QObject{parent}
+{
+}
+
+const QString& ImagesDirectory::directoryPath() const
+{
+  return m_directoryPath;
+}
+
+void ImagesDirectory::setDirectoryPath(const QString& newDirectoryPath)
+{
+  if (m_directoryPath == newDirectoryPath)
+    return;
+  m_directoryPath = newDirectoryPath;
+  emit directoryPathChanged();
+
+  loadImages();
+}
+
+const std::vector<QImage>& ImagesDirectory::images() const
+{
+  return m_images;
+}
+
+void ImagesDirectory::setImages(const std::vector<QImage>& newImages)
+{
+  if (m_images == newImages)
+    return;
+  m_images = newImages;
+  emit imagesChanged();
+}
+
+void ImagesDirectory::loadImages()
+{
+  std::vector<QImage> images;
+
+  QDir dir{m_directoryPath};
+
+  for (const QFileInfo& fi: dir.entryInfoList(QDir::Files))
+  {
+    QImage image{QString{fi.absoluteFilePath()}};
+    if (!image.isNull())
+    {
+      images.push_back(image);
+    }
+  }
+
+  setImages(images);
+}
+
+} // namespace wsgui
---
+++ src/imagesdirectory.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <QImage>
+#include <QObject>
+#include <QQmlEngine>
+
+namespace wsgui
+{
+
+class ImagesDirectory : public QObject
+{
+  Q_OBJECT
+  Q_PROPERTY(QString directoryPath READ directoryPath WRITE setDirectoryPath NOTIFY
+                 directoryPathChanged FINAL)
+  Q_PROPERTY(std::vector<QImage> images READ images NOTIFY imagesChanged FINAL)
+  QML_ELEMENT
+public:
+  explicit ImagesDirectory(QObject* parent = nullptr);
+
+  const QString& directoryPath() const;
+  void setDirectoryPath(const QString& newDirectoryPath);
+
+  const std::vector<QImage>& images() const;
+
+signals:
+  void directoryPathChanged();
+  void imagesChanged();
+
+private:
+  void setImages(const std::vector<QImage>& newImages);
+  void loadImages();
+
+  QString m_directoryPath;
+  std::vector<QImage> m_images;
+};
+
+} // namespace wsgui
