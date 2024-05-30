a6242b007df852fcabe87f420354e1334e5e514f
Daniel Spiessberger <80575371+Spiessberger@users.noreply.github.com>
2024-05-30 09:23:34 +0200

    add assignment operator

--- src/measurement.h
+++ src/measurement.h
@@ -18,12 +18,19 @@ template<typename T, Unit unit_>
 struct Measurement
 {
   Measurement() = default;
-  Measurement(const T& value_)
+  explicit Measurement(const T& value_)
       : value(value_)
   {
   }
 
   T value = {};
+
+  Measurement& operator=(const T& value)
+  {
+    this->value = value;
+    return *this;
+  }
+
   static const Unit unit = unit_;
 };
 
