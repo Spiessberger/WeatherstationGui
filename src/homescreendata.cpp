e337226d4c4824c3f983919fcddc55f366fc7ea5
Daniel Spiessberger <daniel.spiessberger@sequality.at>
2024-07-26 11:17:17 +0200

    add current date and time

--- src/homescreendata.cpp
+++ src/homescreendata.cpp
@@ -10,6 +10,9 @@ HomeScreenData::HomeScreenData(const WeatherStationData& weatherStationData, QOb
     : QObject(parent)
     , m_weatherStationData(weatherStationData)
 {
+  connect(&m_dateTimeUpdateTimer, &QTimer::timeout, this, &HomeScreenData::updateDateTime);
+  updateDateTime();
+  m_dateTimeUpdateTimer.start(1s);
 }
 
 void HomeScreenData::setQmlInstance(HomeScreenData* qmlInstance)
@@ -17,6 +20,40 @@ void HomeScreenData::setQmlInstance(HomeScreenData* qmlInstance)
   Q_ASSERT(qmlInstance != nullptr);
   Q_ASSERT(HomeScreenData::qmlInstance == nullptr);
   HomeScreenData::qmlInstance = qmlInstance;
+}
+}
+
+QString HomeScreenData::date() const
+{
+  return m_date.toString();
+}
+
+void HomeScreenData::setDate(const QDate& newDate)
+{
+  if (m_date == newDate)
+    return;
+  m_date = newDate;
+  emit dateChanged();
+}
+
+QString HomeScreenData::time() const
+{
+  return m_time.toString();
+}
+
+void HomeScreenData::setTime(const QTime& newTime)
+{
+  if (m_time == newTime)
+    return;
+  m_time = newTime;
+  emit timeChanged();
+}
+
+void HomeScreenData::updateDateTime()
+{
+  const QDateTime currentDateTime = QDateTime::currentDateTime();
+  setTime(currentDateTime.time());
+  setDate(currentDateTime.date());
 }
 
 } // namespace data
--- src/homescreendata.h
+++ src/homescreendata.h
@@ -2,6 +2,7 @@
 
 #include <QObject>
 #include <QQmlEngine>
+#include <QTimer>
 
 #include "weatherstationdata.h"
 
@@ -15,15 +16,31 @@ class HomeScreenData : public QObject
   Q_OBJECT
   QML_ELEMENT
   QML_SINGLETON
+
+  Q_PROPERTY(QString date READ date NOTIFY dateChanged FINAL)
+  Q_PROPERTY(QString time READ time NOTIFY timeChanged FINAL)
 public:
   explicit HomeScreenData(const WeatherStationData& weatherStationData, QObject* parent = nullptr);
 
   static void setQmlInstance(HomeScreenData* qmlInstance);
 
+  QString date() const;
+  QString time() const;
+
 signals:
+  void dateChanged();
+  void timeChanged();
 
 private:
   static HomeScreenData* qmlInstance;
+
+  void setDate(const QDate& newDate);
+  void setTime(const QTime& newTime);
+  void updateDateTime();
+
+  QDate m_date;
+  QTime m_time;
+  QTimer m_dateTimeUpdateTimer;
   const WeatherStationData& m_weatherStationData;
 };
 
