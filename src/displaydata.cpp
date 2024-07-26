75d91c2057ed0d3d1a4929deddbeb65249ccd53d
Daniel Spiessberger <daniel.spiessberger@sequality.at>
2024-07-26 11:10:00 +0200

    add qml class that holds value and unit

---
+++ src/displaydata.cpp
@@ -0,0 +1,57 @@
+#include "displaydata.h"
+
+namespace wsgui
+{
+namespace data
+{
+
+DisplayData::DisplayData(QObject* parent)
+    : QObject(parent)
+{
+}
+
+const QString& DisplayData::value() const
+{
+  return m_value;
+}
+
+QString DisplayData::unit() const
+{
+  switch (m_unit)
+  {
+    case Unit::KilometersPerHour:
+      return QStringLiteral("km/h");
+    case Unit::Percent:
+      return QStringLiteral("%");
+    case Unit::DegreeCelsius:
+      return QStringLiteral("ºC");
+    case Unit::Degree:
+      return QStringLiteral("º");
+    case Unit::MilliMeter:
+      return QStringLiteral("mm");
+    case Unit::None:
+    default:
+      return QStringLiteral("");
+  }
+}
+
+void DisplayData::setValue(const QString& newValue)
+{
+  if (m_value == newValue)
+    return;
+
+  m_value = newValue;
+  emit valueChanged();
+}
+
+void DisplayData::setUnit(Unit newUnit)
+{
+  if (m_unit == newUnit)
+    return;
+
+  m_unit = newUnit;
+  emit unitChanged();
+}
+
+} // namespace data
+} // namespace wsgui
---
+++ src/displaydata.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <QObject>
+#include <QQmlEngine>
+
+#include "measurement.h"
+
+namespace wsgui
+{
+namespace data
+{
+
+class DisplayData : public QObject
+{
+  Q_OBJECT
+  QML_ANONYMOUS
+
+  Q_PROPERTY(QString value READ value NOTIFY valueChanged FINAL)
+  Q_PROPERTY(QString unit READ unit NOTIFY unitChanged FINAL)
+
+public:
+  explicit DisplayData(QObject* parent = nullptr);
+
+  template<Unit unit>
+  void setMeasurement(const Measurement<float, unit>& measurement);
+
+  const QString& value() const;
+  void setValue(const QString& newValue);
+
+  QString unit() const;
+  void setUnit(Unit newUnit);
+
+signals:
+  void valueChanged();
+  void unitChanged();
+
+private:
+  QString m_value;
+  Unit m_unit = Unit::None;
+};
+
+template<Unit u>
+void DisplayData::setMeasurement(const Measurement<float, u>& measurement)
+{
+  setValue(QString::number(measurement.value, 'f', 1));
+  setUnit(u);
+}
+
+} // namespace data
+} // namespace wsgui
