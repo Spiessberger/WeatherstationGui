ae03ec27e1d4ba703b4c514e51646ab5c1b0cea6
Daniel Spiessberger <80575371+Spiessberger@users.noreply.github.com>
2025-12-05 07:36:24 +0100

    add logging

--- lib/wsgui_core/downloader.cpp
+++ lib/wsgui_core/downloader.cpp
@@ -10,18 +10,23 @@ QtPromise::QPromise<QByteArray> Downloader::startDownload(const QUrl& url)
   return QtPromise::QPromise<QByteArray>{
       [this, url](const auto& resolve, const auto& reject)
       {
+        qDebug() << "starting download" << url;
         const QNetworkRequest request{url};
         QNetworkReply* reply = m_net.get(request);
 
         QObject::connect(reply, &QNetworkReply::errorOccurred,
                          [=](QNetworkReply::NetworkError error)
                          {
+                           qWarning()
+                               << "error occured for download" << url << error;
                            reject(error);
                            reply->deleteLater();
                          });
         QObject::connect(reply, &QNetworkReply::sslErrors,
                          [=](const QList<QSslError>& errors)
                          {
+                           qWarning()
+                               << "ssl error occured for download" << url;
                            reject(errors);
                            reply->deleteLater();
                          });
@@ -31,10 +36,13 @@ QtPromise::QPromise<QByteArray> Downloader::startDownload(const QUrl& url)
                          {
                            if (reply->error() == QNetworkReply::NoError)
                            {
+                             qDebug() << "finished download" << url;
                              resolve(reply->readAll());
                            }
                            else
                            {
+                             qWarning() << "error occured for download" << url
+                                        << reply->error();
                              reject(reply->error());
                            }
 
